3fcc200d678ec95ce3488accf1f68936f8b35401
snowlight-aemt <82430645+snowlight-aemt@users.noreply.github.com>
2022-12-23 17:12:54 +0900

    주석 정리

--- app.js
+++ app.js
@@ -29,7 +29,7 @@ sequelize.sync({ force:true }) // 테이블 삭제 후 다시 생성
     .catch((err) => {
         console.error(err);
     });
-// 6장 참고 미들웨어
+    
 app.use(morgan('dev')); // combined
 app.use(express.static(path.join(__dirname, 'public'))); // static resource location
 app.use(express.json());                            // json 요청 (req.body 를 ajax json 요청으로 부터)
@@ -44,15 +44,13 @@ app.use(session({
         secure: false,
     }
 }));
+
 // passport 는 session 미들웨어 밑에서 생성해야만 한다. !!
-app.use(passport.initialize()); // req.user, req.login, req.isAuth.., req.logout
-// router/page.js ==> login, isAuto..., user,...
-// 위에 initialize 에서 login 를 체크가 완료되면
-// passport.session 로 세션에 저장한다.
-app.use(passport.session()); // connect.sid 라는 이름으로 세션 쿠키가 브라우저로 전송.
-// 브라우저 connect.sid=1234592034923840
+app.use(passport.initialize());
+app.use(passport.session());
 
 app.use('/auth', authRouter);
+app.
 
 // 미들웨어는 next(error) 를 해야지만 다음 미들웨어로 이동한다.
 // 404 NOT FOUND
@@ -64,15 +62,9 @@ app.use((req, res, next) => {
 app.use((err, req, res, next) => {
     res.locals.message = err.message;
     // 배포 모드 err 를 숨김.
-    res.locals.error = process.env.NODE_ENV !== 'production' ? err : {}; // 에러 로그를 서비스한테 넘겨.
+    res.locals.error = process.env.NODE_ENV !== 'production' ? err : {};
     res.status(err.status || 500);
     res.render('error');
-    // views/error.html 위치를 찾기 위한 설정. res.render(.....);)
-        // app.set('view engine', 'html');
-        // nunjucks.configure('views', {
-            // express: app,
-            // watch: true,
-        // });    
 });
 
 app.listen(app.get('port'), () => {
