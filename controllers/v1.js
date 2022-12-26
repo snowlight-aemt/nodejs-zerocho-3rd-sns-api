f5dbe6a6ed1be9ebd9e705a5540d9db91b450ae8
snowlight-aemt <82430645+snowlight-aemt@users.noreply.github.com>
2022-12-26 23:45:34 +0900

    Add - API Search Posts

--- controllers/v1.js
+++ controllers/v1.js
@@ -1,6 +1,10 @@
 const User = require('../models/user');
 const Domain = require('../models/domain');
+const Post = require('../models/post');
+const Hashtag = require('../models/hashtag');
+
 const jwt = require('jsonwebtoken');
+
 exports.createToken = async (req, res) => {
     const { clientSecret } = req.body;
 
@@ -46,3 +50,89 @@ exports.createToken = async (req, res) => {
 exports.tokenTest = async (req, res) => {
     res.json(res.locals.decoded);
 }
+
+exports.getMyPosts = async (req, res, next) => {
+    console.log(`user : ${req.user}`);
+    console.dir(`decoded : ${res.locals.decoded}`);
+    try {
+        const posts = await Post.findAll({
+            where: { userId: res.locals.decoded.id }
+        });
+        return res.status(200).json({
+            code: 200,
+            payload: posts,
+        });
+    } catch (error) {
+        return res.status(500).json({
+            code: 500,
+            message: '서버 에러',
+        });
+    }
+};
+
+exports.getPostsByHashtag = async (req, res, next) => {
+    try {
+        const hashtag = await Hashtag.findOne({
+            where: { title: req.params.title }
+        });
+        if (!hashtag) {
+            return res.status(404).json({
+                code: 404,
+                message: '검색 결과가 없습니다.',
+            });
+        }
+
+        const posts = await hashtag.getPosts();
+        if (posts.length === 0) {
+            return res.status(404).json({
+                code: 404,
+                message: '검색 결과가 없습니다.',
+            });
+        }
+
+        return res.status(200).json({
+            code: 200,
+            payload: posts,
+        });
+    } catch (error) {
+        console.error(error);
+        return res.status(500).json({
+            code: 500,
+            message: '서버 에러',
+        })        
+    }
+}
+
+// ----
+// const hashtag = await Hashtag.findOne({
+//     where: { title: req.params.title }
+// });
+// const posts = await hashtag.getPosts();
+
+// SELECT `id`, `title`, `createdAt`, `updatedAt` FROM `hashtags` AS `Hashtag` WHERE `Hashtag`.`title` = 'Node';
+//
+// SELECT `Post`.`id`, `Post`.`content`, `Post`.`img`, `Post`.`createdAt`, `Post`.`updatedAt`, `Post`.`UserId`, `Hashtags`.`id` AS `Hashtags.id`, `Hashtags`.`title` AS `Hashtags.title`, `Hashtags`.`createdAt` AS `Hashtags.createdAt`, `Hashtags`.`updatedAt` AS `Hashtags.updatedAt`, `Hashtags->PostHashtag`.`createdAt` AS `Hashtags.PostHashtag.createdAt`, `Hashtags->PostHashtag`.`updatedAt` AS `Hashtags.PostHashtag.updatedAt`, `Hashtags->PostHashtag`.`HashtagId` AS `Hashtags.PostHashtag.HashtagId`, `Hashtags->PostHashtag`.`PostId` AS `Hashtags.PostHashtag.PostId`
+// FROM `posts` AS `Post`
+// INNER JOIN
+//     `PostHashtag` AS `Hashtags->PostHashtag`
+// INNER JOIN `hashtags` AS `Hashtags`
+// ON `Hashtags`.`id` = `Hashtags->PostHashtag`.`HashtagId`
+// ON `Post`.`id` = `Hashtags->PostHashtag`.`PostId`
+// AND `Hashtags`.`title` = 'node'
+//
+
+
+// ----
+// const posts = await Post.findAll({
+//     include: {
+//         model: Hashtag,
+//         where: { title: hashtag.title },
+//     }
+// });
+
+// SELECT `id`, `title`, `createdAt`, `updatedAt` FROM `hashtags` AS `Hashtag` WHERE `Hashtag`.`title` = 'Node';
+// SELECT `Post`.`id`, `Post`.`content`, `Post`.`img`, `Post`.`createdAt`, `Post`.`updatedAt`, `Post`.`UserId`, `PostHashtag`.`createdAt` AS `PostHashtag.createdAt`, `PostHashtag`.`updatedAt` AS `PostHashtag.updatedAt`, `PostHashtag`.`HashtagId` AS `PostHashtag.HashtagId`, `PostHashtag`.`PostId` AS `PostHashtag.PostId`
+// FROM `posts` AS `Post`
+// INNER JOIN `PostHashtag` AS `PostHashtag`
+// ON `Post`.`id` = `PostHashtag`.`PostId`
+// AND `PostHashtag`.`HashtagId` = 1;
--- routes/v1.js
+++ routes/v1.js
@@ -1,9 +1,15 @@
 const express = require('express');
 const { verifyToken } = require('../middlewares');
-const { createToken, tokenTest } = require('../controllers/v1');
+const { createToken, tokenTest, getMyPosts, getPostsByHashtag } = require('../controllers/v1');
 const router = express.Router();
 
 router.post('/token', createToken); // req.body.clientSecret
 router.get('/test', verifyToken, tokenTest);
 
+router.get('/post/my', verifyToken, getMyPosts);
+router.get('/post/hashtag/:title', verifyToken, getPostsByHashtag);
+
 module.exports = router;
+
+// TODO 나의 게시글 리스트
+// TODO 해쉬태그 검색 관련 게시글 리스트
